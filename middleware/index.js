8116f881738b6d5dd97a5eddbc3b71bcfd4ba5dd
Cristhian <cris2014971130@gmail.com>
2020-12-03 09:56:32 -0500

    Se agrego la lectura de archivo txt

--- middleware/index.js
+++ middleware/index.js
@@ -7,10 +7,53 @@ const hbs = require("hbs");
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 
+const readLastLines = require('read-last-lines');
+
+
+let serverAStatus;
+let serverBStatus;
+var server1;
+var server2;
+let time='';
+
+setInterval(()=>{
+	readLastLines.read('info.txt', 20).then((lines) => {
+		let data = lines.split('\n');
+		for (var i = 0; i < data.length; i++) {
+			if(data[i] == 'Server1'){
+				if(data[i + 1] === '')
+					serverAStatus = 'FAIL';
+				else
+					serverAStatus = 'OK';
+				i++;
+			}else if(data[i] == 'Server2'){
+        serverBStatus = 'OK';
+        if(data[i + 1] === '')
+					serverBStatus = 'FAIL';
+				else
+					serverBStatus = 'OK';
+				i++;
+			}
+		}
+	});
+},1000);
+
+
 app.get("/", (req, res) => {
+    if(serverAStatus =='OK'){
+      server1="Servidor Funcionando";
+    }
+    if(serverAStatus =='FAIL'){
+      server1="Servidor No Funcionando";
+    }else if(serverBStatus=='FAIL'){
+      server2="Servidor No Funcionando";
+    }
+    if(serverBStatus=='OK'){
+      server2="Servidor Funcionando";
+    }
   res.render("index", {
-    hour: 12,
-    date: "12/21",
+      server1: server1,
+      server2: server2,
   });
 });
 
--- middleware/info.sh
+++ middleware/info.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watch -n 0.5 "(date '+TIME:%H:%M:%S'; echo 'Server1' ; ping 192.168.0.11:3000; echo '' ;echo 'Server2' ; curl 192.168.0.7:3000 ; echo '') >> info.txt"
+watch -n 0.5 "(date '+TIME:%H:%M:%S'; echo 'Server1' ; curl --connect-timeout 5 192.168.0.11:3000; echo '' ;echo 'Server2' ; curl  --connect-timeout 5 192.168.0.12:4000; echo '') >> info.txt"
